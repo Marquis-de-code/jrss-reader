18e80a5e4a0965d01877c3468e8d9ac37ed3c36c
Marquis de Code <alexander.mitchell.marquis@gmail.com>
2023-06-25 17:17:06 -0600

    Init commit that lays out the skeleton structure at the module level for JRSS Reader using both JavaFX and Spring Boot. Uses simple hello world to prove cross module compatibility.

---
+++ jrss-client/src/main/java/code/de/marquis/jrss/client/JrssClient.java
@@ -0,0 +1,10 @@
+package code.de.marquis.jrss.client;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class JrssClient {
+	public String getHelloWorldText() {
+		return "Hello World!";
+	}
+}
